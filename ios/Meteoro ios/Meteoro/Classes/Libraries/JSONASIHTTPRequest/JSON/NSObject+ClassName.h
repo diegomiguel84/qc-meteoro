aa33dedc799763dc33e79c4c2711bd72cf2b02b1
Sofrecom <diegomiguel84@yahoo.com>
2012-08-07 12:33:14 -0300

    Remove duplicated directory

--- ios/Meteoro ios/Meteoro/Classes/Libraries/JSONASIHTTPRequest/JSON/NSObject+ClassName.h
+++
@@ -1,6 +0,0 @@
-@interface NSObject (NSObjectClassNameReflection)
-#if (TARGET_OS_IPHONE)
-- (NSString *)className;
-+ (NSString *)className;
-#endif
-@end
--- ios/Meteoro ios/Meteoro/Classes/Libraries/Patches/NSMutableSetNonRetain.h
+++
@@ -1,9 +0,0 @@
-const void* MRetainNoOp(CFAllocatorRef allocator, const void *value);
-void MReleaseNoOp(CFAllocatorRef allocator, const void *value);
-
-@interface NSMutableSet (SetNonRetaining)
-/**
- * Creates a mutable set where the containing objects won't receive retain/release messages.
- */
-+ (NSMutableSet*) setNonRetaining;
-@end
